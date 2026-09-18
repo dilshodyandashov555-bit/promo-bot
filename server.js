9b6107464d07e80a0a1fcaa99b5f83713d52ab45
dilshod yandashov <dilshodyandashov555@gmail.com>
2026-09-18 23:26:45 +0500

    Koddagi xato tuzatildi

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const PORT = process.env.PORT || 3000;
 
 // Odamlar sonini va linklarni xotirada saqlash uchun oddiy baza (O'zgaruvchi)
 let clickStats = {
-  "buxoro": 0,
-  "navoiy": 0
+    "buxoro": 0,
+    "navoiy": 0
 };
 
 // Asosiy ilova yoki maqsadli sayt linki (Siz yo'naltirmoqchi bo'lgan havola)
@@ -14,23 +14,23 @@ const TARGET_URL = "https://google.com";
 // 1. Havolani bosganda ishlaydigan bo'lim
 app.get('/r/:linkId', (req, res) => {
     const linkId = req.params.linkId; // masalan: buxoro yoki navoiy
-    
+
     // Agar baza ichida bu ID bo'lsa, sonini 1 taga oshiramiz
     if (clickStats[linkId] !== undefined) {
         clickStats[linkId] += 1;
     } else {
         clickStats[linkId] = 1; // Yangi ID bo'lsa bazaga qo'shadi
     }
-    
+
     // Foydalanuvchini asosiy ilova yuklash sahifasiga yuborish
     res.redirect(TARGET_URL);
 });
 
-// 2. Kliklar statistikasini ko'rish bo'limi (Masalan: ://render.com)
+// 2. Kliklar statistikasini ko'rish bo'limi (Masalan: //://render.com)
 app.get('/stats', (req, res) => {
     res.json({
-        tizim: "Havolalar statistikasi",
-        ma'lumotlar: clickStats
+        tizim: "Havolar statistikasi",
+        statistika: clickStats
     });
 });
 
